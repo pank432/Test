34000d3f992636969525cdb249b64e6af9c6bae4
pank432 <pank432@gmail.com>
2017-05-18 07:41:02 +0000

    test

--- server.js
+++ server.js
@@ -3,9 +3,30 @@ var bodyParser = require('body-parser');
 var mongoose=require('mongoose');
 var app = express();
 app.use(bodyParser.json());
-//mongoose.connect('mongodb://localhost:27017/mittens');
+app.use(express.static('public')); 
+mongoose.connect('mongodb://localhost:27017/mittens');
+
+
+var Meow = mongoose.model('Meow', {
+ text: String
+  });
+
+  app.get('/meows',function(req,res,next){
+    Meow.find({},function(err,meows){
+      return res.json(meows);
+    });
+  })
+  app.post('/meows', function(req, res, next){	
+		var newMeow = new Meow({
+			text: req.body.newMeow		
+		});
+		newMeow.save(function(err){
+			return res.send("Added Successfully");
+	});	
+	});
 
 app.listen(4000, function () {
-  console.log('Example app listening on port 4000!!')
+  console.log('Example app listening on port 4000!')
 });
+
 
