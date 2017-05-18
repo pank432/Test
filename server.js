55b94ea17beda6de023e735631d364510e59b0a8
pank432 <pank432@gmail.com>
2017-05-18 07:40:51 +0000

    changes added

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var bodyParser = require('body-parser');
 var mongoose=require('mongoose');
 var app = express();
 app.use(bodyParser.json());
-mongoose.connect('mongodb://localhost:27017/mittens');
+//mongoose.connect('mongodb://localhost:27017/mittens');
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(4000, function () {
+  console.log('Example app listening on port 4000!!')
 });
 
