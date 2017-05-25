d080c459a110c9213ca7883a5bd61de94b2c2a7d
Github@28 <pank432@gmail.com>
2017-05-25 07:29:15 +0000

    Changes done

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose=require('mongoose');
+var bcrypt = require('bcryptjs');
 var app = express();
 app.use(bodyParser.json());
 app.use(express.static('public')); 
 mongoose.connect('mongodb://localhost:27017/mittens');
 
+var User = mongoose.model('User', {
+ username: String,
+ password: String
+  });
 
 var Meow = mongoose.model('Meow', {
  text: String
@@ -23,7 +28,29 @@ var Meow = mongoose.model('Meow', {
 		newMeow.save(function(err){
 			return res.send("Added Successfully");
 	});	
+  app.put('/meows/remove',function(req,res,next){
+    var meowId = req.body.meow._id;
+    Meow.remove({_id:meowId},function(err){
+      return res.send("Deleted Successfully");
+    });
+  });
 	});
+  app.post('/users', function(req, res, next){		
+
+      bcrypt.genSalt(10, function(err,salt){
+        bcrypt.hash(req.body.password,salt,function(err,hash){
+			var newUser= new User({
+				username: req.body.username,
+				password: hash
+			});
+			newUser.save( function(err){
+			return res.send();
+	});
+        });
+      });		
+	}) ;	
+
+ 
 
 app.listen(4000, function () {
   console.log('Example app listening on port 4000!')
